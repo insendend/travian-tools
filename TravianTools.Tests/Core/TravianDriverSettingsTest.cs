eb50de7e50ae42f4b0c8560ead9bf21107e2dbe2
and <insendend@gmail.com>
2020-08-11 20:55:10 +0300

    add tests for <TravianDriverSettings>

---
+++ TravianTools.Tests/Core/TravianDriverSettingsTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using TravianTools.Core.Driver;
+using Xunit;
+
+namespace TravianTools.Tests.Core
+{
+    public class TravianDriverSettingsTest
+    {
+        [Theory]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void ThrowInvalidDirectoryWithDriverLocation(string location)
+        {
+            var settings = GetValidSettings();
+            settings.ChromeDriverLocation = location;
+            
+            Assert.Throws<DirectoryNotFoundException>(() => settings.ThrowIfNotValid());
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        [InlineData("123")]
+        [InlineData("♣♠♦•☻")]
+        [InlineData("/wswsws/wssw")]
+        public void ThrowInvalidHostUrl(string url)
+        {
+            var settings = GetValidSettings();
+            settings.HostUrl = url;
+            
+            Assert.Throws<ArgumentException>(() => settings.ThrowIfNotValid());
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ThrowInvalidLogin(string login)
+        {
+            var settings = GetValidSettings();
+            settings.Login = login;
+            
+            Assert.Throws<ArgumentNullException>(() => settings.ThrowIfNotValid());
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ThrowInvalidPassword(string pwd)
+        {
+            var settings = GetValidSettings();
+            settings.Password = pwd;
+            
+            Assert.Throws<ArgumentNullException>(() => settings.ThrowIfNotValid());
+        }
+
+        private TravianDriverSettings GetValidSettings()
+        {
+            return new TravianDriverSettings
+            {
+                ChromeDriverLocation = ".",
+                HostUrl = "https://test.example",
+                Login = "login",
+                Password = "pwd"
+            };
+        }
+    }
+}
