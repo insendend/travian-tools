ede80459dcbb5ff2e2bef0916c1ab6a2d6dac5c3
and <insendend@gmail.com>
2020-08-12 20:59:21 +0300

    add point extensions

---
+++ TravianTools.Core/Extensions/PointExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TravianTools.Core.Extensions
+{
+    public static class PointExtensions
+    {
+        public static IEnumerable<Point> GetNearestPoints(this Point self, int radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("negative radius value", nameof(radius));
+            }
+            
+            var minX = self.X - radius;
+            var maxX = self.X + radius;
+            var minY = self.Y - radius;
+            var maxY = self.Y + radius;
+
+            for (var x = minX; x <= maxX; x++)
+            {
+                for (var y = minY; y <= maxY; y++)
+                {
+                    yield return new Point(x, y);
+                }
+            }
+        }
+        
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.Shuffle(new Random());
+        }
+        
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            
+            if (rng is null)
+            {
+                throw new ArgumentNullException(nameof(rng));
+            }
+            
+            return source.ShuffleIterator(rng);
+        }
+        
+        private static IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> source, Random rng)
+        {
+            var buffer = source.ToList();
+            for (var i = 0; i < buffer.Count; i++)
+            {
+                var j = rng.Next(i, buffer.Count);
+                yield return buffer[j];
+                buffer[j] = buffer[i];
+            }
+        }
+        
+        public static double GetDistance(this Point self, Point to)
+        {
+            var deltaX = Delta(to.X, self.X);
+            var deltaY = Delta(to.Y, self.Y);
+            var distance = Math.Sqrt(deltaX + deltaY);
+            var rDistance = Math.Round(distance, 1);
+
+            return rDistance;
+        }
+        
+        private static double Delta(int c1, int c2, int size = 400)
+        {
+            return Math.Pow((c1 - c2 + (3 * size + 1)) % (2 * size + 1) - size, 2);
+        }
+    }
+}
---
+++ TravianTools.Tests/Core/PointExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using TravianTools.Core.Extensions;
+using Xunit;
+
+namespace TravianTools.Tests.Core
+{
+    public class PointExtensionsTest
+    {
+        [Theory]
+        [InlineData(44, 12, 44, 15, 3)]
+        [InlineData(44, 12, 43, 13, 1.4)]
+        [InlineData(44, 12, 46, 14, 2.8)]
+        [InlineData(44, 12, 4, 32, 44.7)]
+        [InlineData(44, 12, -56, 15, 100)]
+        [InlineData(44, 12, -55, 2, 99.5)]
+        [InlineData(44, 12, 71, -89, 104.5)]
+        [InlineData(44, 12, -42, -5, 87.7)]
+        public void CorrectDistance(int fromX, int fromY, int toX, int toY, double expectedDistance)
+        {
+            var pointFrom = new Point(fromX, fromY);
+            var pointTo = new Point(toX, toY);
+
+            var actualDistance = pointFrom.GetDistance(pointTo);
+            
+            Assert.Equal(expectedDistance, actualDistance);
+        }
+    }
+}
